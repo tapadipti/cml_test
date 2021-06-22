ab35eaa5f6d9959400eb4a8b68e2a76fe110bd65
tapadipti <tapadipti@gmail.com>
2021-06-22 13:27:49 +0530

    Initial commit; n_est=10; max_f=10

---
+++ train.py
@@ -0,0 +1,30 @@
+# Prepare the dataset
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+df=pd.read_csv('heart.csv')
+
+dataset=pd.get_dummies(df,columns=['sex','cp','fbs','restecg','exang','slope','ca','thal'])
+
+standardscaler=StandardScaler()
+columns_to_scale=['age','trestbps','chol','thalach','oldpeak']
+dataset[columns_to_scale]=standardscaler.fit_transform(dataset[columns_to_scale])
+
+y=dataset['target']
+X=dataset.drop(['target'],axis=1)
+
+# Train
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+n_estimators, max_features = 10, 10
+rf_classifier=RandomForestClassifier(n_estimators=n_estimators, max_features=max_features)
+scores=cross_val_score(rf_classifier,X,y,cv=10)
+score = scores.mean()
+
+print("Scores: ", scores)
+print("Score: ", score)
+
+# Plot the scores
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], scores, color='lightblue', linewidth=3)
+plt.savefig("scores.png")
